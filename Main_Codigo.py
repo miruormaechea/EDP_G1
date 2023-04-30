81caa8d0a47b871eafe6a943920052d8f14ecb42
miruormaechea <mormaecheagraiver@itba.edu.ar>
2023-04-30 08:32:30 -0300

    Buenas, estuve viendo los comments de Ian, modifique varias cosas. Porfavor mirenlas.

--- Main_Codigo.py
+++ Main_Codigo.py
@@ -1,11 +1,12 @@
-import os
-import csv
-from Validadores import *
-from datetime import datetime
-from time import sleep
+import os  # esta libreria es la que usamos para limpiar la pantalla
+import csv  # esta libreria es para las hojas de calculo
+from Funciones import *  # este import nos trae la parte del codigo con todos los validadores
+from datetime import datetime  # la usamos para las fechas
+from time import sleep  # la usamos como "cooldown" de pantalla
 
 
 class Cliente():
+    # La clase cliente tiene un constructor con los parametros relevantes (teniendo en cuenta la abstraccion)
     def __init__(self, nombre, apellido, dni, mail, calle, altura, telefono, nombre_usuario, password):
         self.nombre = nombre
         self.apellido = apellido
@@ -55,6 +56,9 @@ def crear_cliente():
 
 
 class Pedido():
+    # Tiene un constructor que inicializa los atributos de un pedido
+    # Tiene un método llamado "escribir_archivo", que escribe la información del pedido en un archivo CSV.
+    # Tiene un método llamado "str" que devuelve una cadena formateada que representa los detalles del pedido
     def __init__(self, nombre_usuario, productos, total):
         self.nombre_usuario = nombre_usuario
         self.productos = productos
@@ -65,11 +69,23 @@ class Pedido():
         return f"Pedido de {self.nombre_usuario} - Total: ${self.total} \nDetalles del pedido: {self.productos[4]} Cupcake/s Sabor: {self.productos[0]} - Relleno: {self.productos[1]} - Cobertura: {self.productos[2]} - Sprinkle: {self.productos[3]}"
 
     def escribir_archivo(self):
-        with open('pedidos.csv', mode='a', newline='') as csv_file:
+        header = ["Fecha", "Hora", "Usuario", "Sabor", "Relleno", "Cobertura", "Sprinkle", "Cantidad de Cupcakes",
+                  "Total"]
+        pedidos_file = "C:/Users/ormae/OneDrive/Documents/pedidos.csv"
+
+        if not os.path.isfile(pedidos_file) or os.path.getsize(pedidos_file) == 0:
+            with open(pedidos_file, "w", newline='') as f:
+                writer = csv.writer(f)
+                writer.writerow(header)
+
+        with open(pedidos_file, mode='a', newline='') as csv_file:
             writer = csv.writer(csv_file)
+            now = datetime.now()
+            if csv_file.tell() == 0:  # Si el archivo esta vacio, entonces se escriben los headers
+                writer.writerow(header)
             writer.writerow(
-                [datetime.now(), self.nombre_usuario, self.productos[4], self.productos[0], self.productos[1],
-                 self.productos[2], self.productos[3],
+                [now.strftime("%Y-%m-%d"), now.strftime("%H:%M"), self.nombre_usuario,
+                 self.productos[0], self.productos[1], self.productos[2], self.productos[3], self.productos[4],
                  self.total])
 
 
@@ -77,7 +93,7 @@ class Tienda():
     lista_pedidos = []
 
     def __init__(self):
-
+        # aca los guardo como diccionario
         self.sabor = {
             "Chocolate": 100,
             "Vainilla": 200,
@@ -122,26 +138,26 @@ class Tienda():
             case 1:
                 cont = 0
                 print("Sabores disponibles:")
-                for sabor,precio in self.sabor.items():
-                    cont+=1
+                for sabor, precio in self.sabor.items():
+                    cont += 1
                     print(f"{cont} {sabor} - ${precio}")
             case 2:
                 cont = 0
                 print("Rellenos disponibles:")
-                for relleno,precio in self.relleno.items():
+                for relleno, precio in self.relleno.items():
                     cont += 1
                     print(f"{cont} {relleno} - ${precio}")
             case 3:
                 cont = 0
                 print("Coberturas disponibles:")
-                for cobertura,precio in self.cobertura.items():
+                for cobertura, precio in self.cobertura.items():
                     cont += 1
                     print(f"{cont} {cobertura} - ${precio}")
             case 4:
                 print("Sprinkles disponibles:")
                 cont = 0
-                for sprinkle,precio in self.sprinkle.items():
-                    cont+=1
+                for sprinkle, precio in self.sprinkle.items():
+                    cont += 1
                     print(f"{cont} {sprinkle} - ${precio}")
 
     def hacer_pedido(self, usuario_pedido, pedido_anterior=None):
@@ -206,7 +222,6 @@ class Tienda():
                     n_sprinkle = input(
                         "Ingrese un número valido de sprinkle que quiera:  ")
 
-
                 cantidad = input("¿Cuantos cupcakes quiere?: ")
                 validado = False
                 while validado == False:
@@ -221,14 +236,14 @@ class Tienda():
                     else:
                         validado = True
 
-                total += self.sabor[list(self.sabor)[int(n_sabor)-1]] * int(cantidad)
-                carrito.append(list(self.sabor)[int(n_sabor)-1])
-                total += self.relleno[list(self.relleno)[int(n_relleno)-1]] * int(cantidad)
-                carrito.append(list(self.relleno)[int(n_relleno)-1])
-                total += self.cobertura[list(self.cobertura)[int(n_cobertura)-1]] * int(cantidad)
-                carrito.append(list(self.cobertura)[int(n_cobertura)-1])
-                total += self.sprinkle[list(self.sprinkle)[int(n_sprinkle)-1]] * int(cantidad)
-                carrito.append(list(self.sprinkle)[int(n_sprinkle)-1])
+                total += self.sabor[list(self.sabor)[int(n_sabor) - 1]] * int(cantidad)
+                carrito.append(list(self.sabor)[int(n_sabor) - 1])
+                total += self.relleno[list(self.relleno)[int(n_relleno) - 1]] * int(cantidad)
+                carrito.append(list(self.relleno)[int(n_relleno) - 1])
+                total += self.cobertura[list(self.cobertura)[int(n_cobertura) - 1]] * int(cantidad)
+                carrito.append(list(self.cobertura)[int(n_cobertura) - 1])
+                total += self.sprinkle[list(self.sprinkle)[int(n_sprinkle) - 1]] * int(cantidad)
+                carrito.append(list(self.sprinkle)[int(n_sprinkle) - 1])
                 carrito.append(cantidad)
                 break
 
@@ -244,46 +259,66 @@ class Tienda():
             input("Presione Enter para volver al menú principal")
 
     def ver_pedidos(self, usuario):
+        header = ["Fecha", "Hora", "Usuario", "Sabor", "Relleno", "Cobertura", "Sprinkle",
+                  "Cantidad de Cupcakes", "Total"]
 
-        os.system('cls' if os.name == 'nt' else 'clear')
-        with open('pedidos.csv', mode='r') as csv_file:
-            existe = False
-            reader = csv.reader(csv_file)
-            pedidos_usuario = []
-            i = 0
-            for row in (reader):
-                if row[1] == usuario:
-                    existe = True
-                    i += 1
-                    print(
-                        f'{i} Fecha: {row[0]} - Total: ${row[7]}- {row[2]} Cupcake/s Sabor: {row[3]} - Relleno: {row[4]} -'
-                        f' Cobertura: {row[5]} - Sprinkle: {row[6]}\n ')
-                    pedidos_usuario.append(row)
-            if existe == False:
-                print(f'Disculpa {usuario} no realizaste ningun pedido todavia')
-            repetir = input(
-                "Presione 1 si desea volver a pedir uno de sus pedidos, cualquier otra letra en caso contrario:")
-            if repetir != "1":
-                input("Presione Enter para volver al menú principal")
-            elif repetir == "1":
-                pedido = input("Ingrese el numero de pedido que quiere repetir: ")
-                while not es_int(pedido):
-                    pedido = input("Ingrese un numero valido de pedido: ")
+        pedidos_file = "C:/Users/ormae/OneDrive/Documents/pedidos.csv"
+
+        try:
+            with open(pedidos_file, mode='r') as csv_file:
                 existe = False
-                for i, row in enumerate(pedidos_usuario):
-                    if i == int(pedido) - 1:
+                reader = csv.DictReader(csv_file, fieldnames=header)
+                pedidos_usuario = []
+                i = 0
+
+                for row in reader:
+                    if row.get("Usuario") == usuario:
                         existe = True
-                        datos_pedido = [row[3], row[4], row[5], row[6]]
-                        # Estoy pasando los datos en orden, Sabor, Relleno, Cobertura, Sprinkle
-                        self.hacer_pedido(usuario, datos_pedido)
+                        i += 1
+                        print(
+                            f'{i} Fecha: {row.get("Fecha")} - Total: ${row.get("Total")} - '
+                            f'Cantidad de cupcakes: {row.get("Cantidad de Cupcakes")} - Sabor: {row.get("Sabor")} '
+                            f'- Relleno: {row.get("Relleno")}  - Cobertura: {row.get("Cobertura")} - Sprinkle: {row.get("Sprinkle")}\n ')
+                        pedidos_usuario.append(row)
+
                 if existe == False:
-                    print("No ingreso un numero valido de pedido")
-                    sleep(2)
+                    print(f'Disculpa {usuario} no realizaste ningun pedido todavia')
+                    sleep(7)
+                else:
+                    pedido = input("Ingrese el numero de pedido que quiere repetir, en caso de que no quiera repetir " \
+                                   "presione cualquier otro numero: ")
+                    while not es_int(pedido):
+                        pedido = input("Ingrese un numero: ")
+                    for i, row in enumerate(pedidos_usuario):
+                        if i == int(pedido) - 1:
+                            existe = True
+                            datos_pedido = [row.get("Sabor"), row.get("Relleno"), row.get("Cobertura"),
+                                            row.get("Sprinkle")]
+                            # aca usamos el get que recomendo Ian
+                            self.hacer_pedido(usuario, datos_pedido)
+                    if existe == False:
+                        print("Volviendo al menu principal")
+                        sleep(10)
+
+        except FileNotFoundError:
+            print(f'Error: el archivo {pedidos_file} no se encuentra')
 
     def iniciar(self):
         inicio = False
         opcion = 0
-        users_file = "C:/Users/ormae/OneDrive/Documents/usuarios.txt"
+        users_file = "C:/Users/ormae/OneDrive/Documents/users.txt"
+        users_data_file = "C:/Users/ormae/OneDrive/Documents/users_data.csv"
+
+        if not os.path.isfile(users_file) or os.path.getsize(users_file) == 0:
+            with open(users_file, "w") as f:
+                pass
+
+        if not os.path.isfile(users_data_file) or os.path.getsize(users_data_file) == 0:
+            with open(users_data_file, "w", newline='') as f:
+                header = ["Nombre de Usuario", "DNI", "Nombre", "Apellido", "Calle", "Altura", "Mail", "Telefono"]
+                writer = csv.writer(f)
+                writer.writerow(header)
+
         while inicio != True:
             opcion = input("Seleccione una opcion:\n1.Iniciar sesion\n2.Crear cuenta\n")
 
@@ -304,46 +339,45 @@ class Tienda():
                     if cont_login != 1:
                         print("Usuario o Contrasena incorrectos.")
 
-
-
             elif opcion == "2":
                 cliente_nuevo = crear_cliente()
 
-                with open(users_file, "r+") as f:
-                    for line in f:
+                with open(users_file, "r") as f:
+                    lines = f.readlines()
+                    for line in lines:
                         if ':' in line:
                             user, pw = line.strip().split(":")
                             if user == cliente_nuevo.nombre_usuario:
                                 print("El usuario ya existe.")
                                 break
-                            else:
-                                with open('users.csv', mode='a', newline='') as csv_file:
-                                    writer = csv.writer(csv_file)
-                                    writer.writerow(
-                                        [cliente_nuevo.nombre_usuario, cliente_nuevo.dni, cliente_nuevo.nombre,
-                                         cliente_nuevo.apellido, cliente_nuevo.calle, cliente_nuevo.altura,
-                                         cliente_nuevo.mail, cliente_nuevo.telefono])
 
-                                f.write(f"{cliente_nuevo.nombre_usuario}:{cliente_nuevo.password}\n")
-                                Tienda.lista_clientes.append(cliente_nuevo)
-                                print("Usuario creado exitosamente.")
+                    else:
+                        with open(users_file, "a") as f:
+                            f.write(f"{cliente_nuevo.nombre_usuario}:{cliente_nuevo.password}\n")
+                            print("Usuario creado exitosamente.")
 
-        while True and inicio == True:
-            self.menu()
-            opcion = input("Seleccione una opción: ")
-            if opcion == "1":  # opcion ver productos
-                self.ver_productos()
-                input("Presione Enter para volver al menu principal.")
-            elif opcion == "2":  # opcion hacer pedido
-                self.hacer_pedido(usuario_login)
-            elif opcion == "3":  # opcion ver pedidos
-                self.ver_pedidos(usuario_login)
-            elif opcion == "4":  # opcion salir
-                print("Gracias por visitar la tienda de cupcakes online!")
-                break
-            else:
-                print("Opción inválida. Por favor ingrese un número del 1 al 4.")
-                input("Presione Enter para continuar")
+                        with open(users_data_file, "a", newline='') as f:
+                            writer = csv.writer(f)
+                            writer.writerow([cliente_nuevo.nombre_usuario, cliente_nuevo.dni, cliente_nuevo.nombre,
+                                             cliente_nuevo.apellido, cliente_nuevo.calle, cliente_nuevo.altura,
+                                             cliente_nuevo.mail, cliente_nuevo.telefono])
+
+            while True and inicio == True:
+                self.menu()
+                opcion = input("Seleccione una opción: ")
+                if opcion == "1":  # opcion ver productos
+                    self.ver_productos()
+                    input("Presione Enter para volver al menu principal.")
+                elif opcion == "2":  # opcion hacer pedido
+                    self.hacer_pedido(usuario_login)
+                elif opcion == "3":  # opcion ver pedidos
+                    self.ver_pedidos(usuario_login)
+                elif opcion == "4":  # opcion salir
+                    print("Gracias por visitar la tienda de cupcakes online!")
+                    break
+                else:
+                    print("Opción inválida. Por favor ingrese un número del 1 al 4.")
+                    input("Presione Enter para continuar")
 
 
 Blue_Velvet_Cupcakes = Tienda()
--- README.md
+++ README.md
@@ -9,25 +9,25 @@ Codigo para el trabajo practico de la Materia Estructuras de Datos y Programacio
 
   
 - Esta muy bueno eso del mail valido, es más, hay un paquete que se llama validate mail que verifica que realmente existe y se puede enviar un mail.
-	- Obviamente no usen eso. Lo digo de dato curioso nomas.
+	- Obviamente no usen eso. Lo digo de dato curioso nomas.  🟩 
 
 - Creo que ya lo habían implementado antes pero lo borraron. Intenten usar switch statements ya que hace el código más legible
 
 - Como ya saben usar diccionarios les diria que cuando generen el txt, usen JSON. Entonces a la hora de buscar la contraseña solo tienen que hacer usuarios.get(nombre_de_usuario) y verificar que lo que devuelva sea la contraseña que buscan ustedes.
 
-- Si van a usar csv les diría que utilicen los headers. Esto les va a permitir usar csv.DictReader() para obtener una lista de diccionarios, así no tienen que llamar a lista[1]. Es mucho más legible lista.get('nombres')
+- Si van a usar csv les diría que utilicen los headers. Esto les va a permitir usar csv.DictReader() para obtener una lista de diccionarios, así no tienen que llamar a lista[1]. Es mucho más legible lista.get('nombres') 🟩
 
 - Tambien les permitiria agregar una funcion en Cliente que devuelva un diccionario con todos los elementos importantes.
 
-- Están guardando el cliente en Tienda.lista_clientes pero nunca lo declaran. Python llama a una función que no existe y tira error
+- Están guardando el cliente en Tienda.lista_clientes pero nunca lo declaran. Python llama a una función que no existe y tira error 🟩
 
 - Tampoco está muy bueno usar esas variables para estos casos, ya que el software que crean se podría usar para múltiples tiendas
 
 	- Si bien es solo para esta tienda, esta bueno generalizarlo para que despues puedan adaptar el codigo a otros proyectos.
 
-- Sobre los pedidos esta bueno eso de repetir pedido. Lo que si, en vez de poner 1 para volver a pedir y preguntar por el numero despues, ofreceria al usuario seleccionar el pedido que quiere repetir y si no pone nada asuman que quiere volver al menu.
+- Sobre los pedidos esta bueno eso de repetir pedido. Lo que si, en vez de poner 1 para volver a pedir y preguntar por el numero despues, ofreceria al usuario seleccionar el pedido que quiere repetir y si no pone nada asuman que quiere volver al menu. 🟩
 
-- No hagan una lista global de pedidos, guardenla en la tienda por lo mismo que dije antes.
+- No hagan una lista global de pedidos, guardenla en la tienda por lo mismo que dije antes.🟩
 
 - Se podria hacer que el usuario mismo guarde su lista de pedidos, asi pasan de una velocidad de consulta de O(N) a un O(1) ya que no tienen que escanear la lista completa cada vez que corren la lista de pedidos.
 
@@ -44,17 +44,17 @@ Codigo para el trabajo practico de la Materia Estructuras de Datos y Programacio
 ### Arreglar
 
 
-- Hay un error en el código para crear usuarios. Esta mal indentado y va a guardar el usuario y contraseña casi infinitas veces.
+- Hay un error en el código para crear usuarios. Esta mal indentado y va a guardar el usuario y contraseña casi infinitas veces.⚠️
 
-- La información que le piden al usuario cuando generan la cuenta se pierde cuando cierran el programa.
+- La información que le piden al usuario cuando generan la cuenta se pierde cuando cierran el programa. ⚠️Si lo guardamos en el csv? No entiendo
 
 - Intenten usar direcciones relativas, no absolutas. El programa me crasheo al intentar generar el usuario
 
-	- En caso de que no exista un archivo, genérenlo
+	- En caso de que no exista un archivo, genérenlo🟩
 
 - En algunas partes del código usan elif para hacer lo que efectivamente es un else.
 
-- Cuando quieren ver si le pasaron un dato pueden hacer if dato:.
+- Cuando quieren ver si le pasaron un dato pueden hacer if dato: 🟩.
 
 	- Ojo con esto si se puede pasar un dato numérico ya que tanto None como 0 va a dar falso
 
